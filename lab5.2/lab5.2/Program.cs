6efbde95ab01f92d649710c307d2a11eaf0d5f00
Denis Litvinets <denis.litvinets@gmail.com>
2020-09-19 23:14:18 +0300

    Third commit

---
+++ lab5.2/lab5.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ac, bc, sum;
+            Console.Write("A = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("B = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("C = ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            ac = Math.Abs(a - c);
+            bc = Math.Abs(b - c);
+            sum = ac + bc;
+            Console.WriteLine("AC = " + ac + ", BC = " + bc + ", AC + BC = " + sum);
+                
+        }
+    }
+}
