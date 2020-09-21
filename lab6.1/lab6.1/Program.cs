fa9c3a8a5ae98528dda4aa506dfbb7b0767c6942
Denis Litvinets <denis.litvinets@gmail.com>
2020-09-21 19:07:47 +0300

    Я начал делать лабу номер 110

---
+++ lab6.1/lab6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace lab6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("a = ");
+            int a = Convert.ToInt32(ReadLine());
+            Write("b = ");
+            int b = Convert.ToInt32(ReadLine());
+            int t; // переменная для хранения значения одной из переменных
+
+            t = a;
+            a = b;
+            b = t;
+
+            Write("Теперь a = " + a + ", b = " + b);
+        }
+    }
+}
