838b20437fb87cbbe39c69c43c297660ba285e41
Denis Litvinets <denis.litvinets@gmail.com>
2020-09-21 19:47:11 +0300

    one more commit 21.09

---
+++ lab6.3/lab6.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace lab6._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("a = ");
+            int a = Convert.ToInt32(ReadLine());
+            Write("b = ");
+            int b = Convert.ToInt32(ReadLine());
+            Write("c = ");
+            int c = Convert.ToInt32(ReadLine());
+            int t; // и сюда добавим коробочку для хранения значения одной из переменных
+
+            t = b;
+            b = c;
+            c = a;
+            a = t;
+
+            Write("Теперь a = " + a + ", b = " + b + ", c = " + c);
+        }
+    }
+}
