c6b1ab862ab0a47e2190f8e235063097a395a48f
Denis Litvinets <denis.litvinets@gmail.com>
2020-09-23 11:59:48 +0300

    23.09 commit

---
+++ lab7.1/lab7.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab7._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение угла в градусах: ");
+            int alpha = Convert.ToInt32(Console.ReadLine());
+            double radians = 180 / System.Math.PI;
+            double angle = alpha / radians;
+            Console.WriteLine("Значение угла в радианах: " + angle);
+
+        }
+    }
+}
---
+++ lab7.2/lab7.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение угла в радианах: ");
+            double angle = Convert.ToDouble(Console.ReadLine());
+            double radians = 180 / System.Math.PI;
+            double degrees = angle * radians;
+            Console.WriteLine("Значение угла в градусах: " + degrees);
+        }
+    }
+}
